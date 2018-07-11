ed17e68327441e5f18546d23012efa01b19da7a1
lotto <lotto_li@163.com>
2018-07-11 09:22:10 +0800

    .

--- platform-font/src/admin/view/home/index.vue
+++ platform-font/src/admin/view/home/index.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="home">
     <h1>{{ msg }}</h1>
-    <router-link to="/">Go to login</router-link>
+    <h2><router-link to="/">Go to login</router-link></h2>
+    <el-button @click="change()">change-lifeCycle</el-button>
+    <lifeCycle v-if="!isdestroy"></lifeCycle>
   </div>
 </template>
 
 <script>
+import lifeCycle from '@/components/lifeCycle'
 export default {
   name: 'home',
   components: {
-
+    lifeCycle
   },
   data () {
     return {
-      msg: 'home'
+      msg: 'home',
+      isdestroy: false
+    }
+  },
+  methods: {
+    change () {
+      this.isdestroy = !this.isdestroy
     }
   }
 }
---
+++ platform-font/src/components/lifeCycle.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="lifeCycle">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lifeCycle',
+  data () {
+    return {
+      msg: 'lifeCycle'
+    }
+  },
+  beforeCreate () {
+    console.info('————beforeCreate')
+  },
+  created () {
+    console.info('————created')
+  },
+  beforeMount () {
+    console.info('————beforeMount')
+  },
+  mounted () {
+    console.info('————mounted')
+  },
+  beforeUpdate () {
+    console.info('————beforeUpdate')
+  },
+  updated () {
+    console.info('————updated')
+  },
+  beforeDestroy () {
+    console.info('————beforeDestroy')
+  },
+  destroyed () {
+    console.info('————destroyed')
+  },
+  methods: {
+    function1 () {
+      console.info('————function1')
+    },
+    function2 () {
+      console.info('————function2')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
