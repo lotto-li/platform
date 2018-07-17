7b5615236bcf3036cb92e100827c9a3f1ba99d09
lotto <lotto_li@163.com>
2018-07-17 17:09:09 +0800

    .

--- platform-font/src/router/index.js
+++ platform-font/src/router/index.js
@@ -12,7 +12,7 @@ export default new Router({
     {
       path: '/',
       name: 'login',
-      component: resolve => require(['@/admin/view/login/index'], resolve)
+      component: resolve => require(['@/admin/view/login/index'], resolve)// 使用懒加载
     },
     {
       path: '/home',
